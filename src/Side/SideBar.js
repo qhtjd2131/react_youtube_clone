b58afdbaaf4eef786721bef8b60e42730b79f14d
qhtjd2131 <qhtjd2131@gmail.com>
2021-11-08 05:20:43 +0900

    sidebar 아이템 컴포넌트 리팩토링

--- src/Side/SideBar.js
+++ src/Side/SideBar.js
@@ -6,26 +6,25 @@ import { LogIn } from "../Header/HeaderBar.js";
 const Line = () => {
   return <div className="line"></div>;
 };
+
+const Items = ({ item }) => {
+  return item.map((i, index) => (
+    <div className="item" key={index}>
+      <div className="itemIcon">{i.image}</div>
+      <div className="itemLabel">{i.title}</div>
+    </div>
+  ));
+};
 const SideBar = () => {
   return (
     <div className="sidebar-container">
       <div className="sidebar">
         <div className="items-wrapper">
-          {data.item_1.map((i, index) => (
-            <div className="item" key={index}>
-              <div className="itemIcon">{i.image}</div>
-              <div className="itemLabel">{i.title}</div>
-            </div>
-          ))}
+          <Items item={data.item_1} />
 
           <Line />
 
-          {data.item_2.map((i, index) => (
-            <div className="item" key={index}>
-              <div className="itemIcon">{i.image}</div>
-              <div className="itemLabel">{i.title}</div>
-            </div>
-          ))}
+          <Items item={data.item_2} />
 
           <Line />
           <div className="side-login-container">
@@ -38,32 +37,18 @@ const SideBar = () => {
 
           <Line />
           <div className="list-label">인기 YOUTUBE</div>
-          {data.item_3.map((i, index) => (
-            <div className="item" key={index}>
-              <div className="itemIcon">{i.image}</div>
-              <div className="itemLabel">{i.title}</div>
-            </div>
-          ))}
+          <Items item={data.item_3} />
           <Line />
 
-          {data.item_4.map((i, index) => (
-            <div className="item" key={index}>
-              <div className="itemIcon">{i.image}</div>
-              <div className="itemLabel">{i.title}</div>
-            </div>
-          ))}
+          <Items item={data.item_4} />
 
           <Line />
           <div className="list-label">YOUTUBE 더보기</div>
 
-          {data.item_5.map((i, index) => (
-            <div className="item" key={index}>
-              <div className="itemIcon">{i.image}</div>
-              <div className="itemLabel">{i.title}</div>
-            </div>
-          ))}
-
+          <Items item={data.item_5} />
           <Line />
+
+          <Items item={data.item_6} />
 
           {/* 정보... */}
         </div>
