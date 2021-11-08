7c9a7ba0e2eef1d678942d0bbd2120d31b877ed7
qhtjd2131 <qhtjd2131@gmail.com>
2021-11-08 17:12:55 +0900

    sidebar styling

--- src/Side/SideBar.js
+++ src/Side/SideBar.js
@@ -1,65 +1,78 @@
-import React from "react";
+import React, { createContext, useContext, useState } from "react";
 import "./SideBar.scss";
 import * as data from "./SideData/data.js";
 import { LogIn } from "../Header/HeaderBar.js";
+
+export const selectedSideItemContext = React.createContext({});
 
 const Line = () => {
   return <div className="line"></div>;
 };
 
 const Items = ({ item, theme }) => {
-  if (theme === "black") {
-    return item.map((i, index) => (
-      <div className="item" key={index}>
-        <div className="itemIcon black">{i.image}</div>
-        <div className="itemLabel">{i.title}</div>
+  const { selectedSideItem, setSelectedSideItem } = useContext(
+    selectedSideItemContext
+  );
+
+  return item.map((i, index) => (
+    <div
+      className={
+        selectedSideItem === i.title
+          ? "side-item selected-side-item"
+          : "side-item"
+      }
+      key={index}
+      onClick={(e) => {
+        setSelectedSideItem(e.target.innerText);
+      }}
+    >
+      <div className={theme === "black" ? "itemIcon black" : "itemIcon"}>
+        {i.image}
       </div>
-    ));
-  } else {
-    return item.map((i, index) => (
-      <div className="item" key={index}>
-        <div className="itemIcon">{i.image}</div>
-        <div className="itemLabel">{i.title}</div>
-      </div>
-    ));
-  }
+      <div className="itemLabel">{i.title}</div>
+    </div>
+  ));
 };
+
 const SideBar = () => {
+  const [selectedSideItem, setSelectedSideItem] = useState(
+    data.item_1[0].title
+  ); //data.item_1[0].title:홈
+
   return (
     <div className="sidebar-container">
       <div className="sidebar">
         <div className="items-wrapper">
-          <Items item={data.item_1} />
+          <selectedSideItemContext.Provider
+            value={{ selectedSideItem, setSelectedSideItem }}
+          >
+            <Items item={data.item_1} />
 
-          <Line />
+            <Line />
 
-          <Items item={data.item_2} />
+            <Items item={data.item_2} />
 
-          <Line />
-          <div className="side-login-container">
-            <div className="side-login-description">
-              로그인하면 동영상에 좋아요를 표시하고 댓글을 달거나 구독할 수
-              있습니다.
+            <Line />
+            <div className="side-login-container">
+              <div className="side-login-description">
+                로그인하면 동영상에 좋아요를 표시하고 댓글을 달거나 구독할 수
+                있습니다.
+              </div>
+              <LogIn />
             </div>
-            <LogIn />
-          </div>
+            <Line />
+            <div className="list-label">인기 YOUTUBE</div>
+            <Items item={data.item_3} theme="black" />
+            <Line />
+            <Items item={data.item_4} />
+            <Line />
+            <div className="list-label">YOUTUBE 더보기</div>
+            <Items item={data.item_5} />
+            <Line />
+            <Items item={data.item_6} />
 
-          <Line />
-          <div className="list-label">인기 YOUTUBE</div>
-          <Items item={data.item_3} theme="black" />
-          <Line />
-
-          <Items item={data.item_4} />
-
-          <Line />
-          <div className="list-label">YOUTUBE 더보기</div>
-
-          <Items item={data.item_5} />
-          <Line />
-
-          <Items item={data.item_6} />
-
-          {/* 정보... */}
+            {/* 정보... */}
+          </selectedSideItemContext.Provider>
         </div>
       </div>
     </div>
--- src/Side/SideData/data.js
+++ src/Side/SideData/data.js
@@ -43,49 +43,73 @@ export const item_3 = [
   {
     title: "음악",
     image: (
-      <img src="https://yt3.ggpht.com/96QoAiiyEzTweSqqNyWc7MfZLZk5xSgcPYuXB0aAdyxelNzU9kAco2GAPV8SeAhj3piG4whRCA=s88-c-k-c0x00ffffff-no-rj" />
+      <img
+        src="https://yt3.ggpht.com/96QoAiiyEzTweSqqNyWc7MfZLZk5xSgcPYuXB0aAdyxelNzU9kAco2GAPV8SeAhj3piG4whRCA=s88-c-k-c0x00ffffff-no-rj"
+        alt=""
+      />
     ),
   },
   {
     title: "스포츠",
     image: (
-      <img src="https://yt3.ggpht.com/jcGRECTvPeggGz4pxqerXrd2PYsjP38t1FeqBz3Vb937GTIuwwgybU4AYY6HwDlQHQt9iaoLlQ=s88-c-k-c0x00ffffff-no-rj" />
+      <img
+        src="https://yt3.ggpht.com/jcGRECTvPeggGz4pxqerXrd2PYsjP38t1FeqBz3Vb937GTIuwwgybU4AYY6HwDlQHQt9iaoLlQ=s88-c-k-c0x00ffffff-no-rj"
+        alt=""
+      />
     ),
   },
   {
     title: "게임",
     image: (
-      <img src="https://yt3.ggpht.com/jQKnCfq2eoBd4EOaubdbvQ5Oe0uQ_CBA-qLRkw46ln1m8JDhkL8oBUwXX63ypSAhOGVj-V8T39g=s88-c-k-c0x00ffffff-no-rj" />
+      <img
+        src="https://yt3.ggpht.com/jQKnCfq2eoBd4EOaubdbvQ5Oe0uQ_CBA-qLRkw46ln1m8JDhkL8oBUwXX63ypSAhOGVj-V8T39g=s88-c-k-c0x00ffffff-no-rj"
+        alt=""
+      />
     ),
   },
   {
     title: "영화",
     image: (
-      <img src="https://yt3.ggpht.com/6lo97rUTO7xhIBXZqLiaW2kA_eMBIEmqc27EqlKLyE4nAY-yzcKBG0Hs0YdUka3gJ629HcwgyzQ=s88-c-k-c0x00ffffff-no-rj" />
+      <img
+        src="https://yt3.ggpht.com/6lo97rUTO7xhIBXZqLiaW2kA_eMBIEmqc27EqlKLyE4nAY-yzcKBG0Hs0YdUka3gJ629HcwgyzQ=s88-c-k-c0x00ffffff-no-rj"
+        alt=""
+      />
     ),
   },
   {
     title: "뉴스",
     image: (
-      <img src="https://yt3.ggpht.com/pZxo_dJtADS-AkEx1mnPpc49paUy_aQywNgHju68VLQRMWJ4z30_thCXaNzERG8Vn74xPS-LLw=s88-c-k-c0x00ffffff-no-rj" />
+      <img
+        src="https://yt3.ggpht.com/pZxo_dJtADS-AkEx1mnPpc49paUy_aQywNgHju68VLQRMWJ4z30_thCXaNzERG8Vn74xPS-LLw=s88-c-k-c0x00ffffff-no-rj"
+        alt=""
+      />
     ),
   },
   {
     title: "실시간",
     image: (
-      <img src="https://yt3.ggpht.com/8D6JlsnvwDZFMdcbjqVji82kggP3aXXbO-yBD0RFrKlp4G1zNt9wcqcVTSPnAI8GuUAbDYQwsg=s88-c-k-c0x00ffffff-no-rj" />
+      <img
+        src="https://yt3.ggpht.com/8D6JlsnvwDZFMdcbjqVji82kggP3aXXbO-yBD0RFrKlp4G1zNt9wcqcVTSPnAI8GuUAbDYQwsg=s88-c-k-c0x00ffffff-no-rj"
+        alt=""
+      />
     ),
   },
   {
     title: "학습",
     image: (
-      <img src="https://yt3.ggpht.com/__Mlk4S9hJAiS34gzzVBqpY-_9tc6m4nIZGk0cMseq5q_Ux51MyhcBWYIPfXmqCCfazU_5hniQ=s88-c-k-c0x00ffffff-no-rj" />
+      <img
+        src="https://yt3.ggpht.com/__Mlk4S9hJAiS34gzzVBqpY-_9tc6m4nIZGk0cMseq5q_Ux51MyhcBWYIPfXmqCCfazU_5hniQ=s88-c-k-c0x00ffffff-no-rj"
+        alt=""
+      />
     ),
   },
   {
     title: "360 동영상",
     image: (
-      <img src="https://yt3.ggpht.com/i3MfCW-U130eRmdBCD5XuYRlu0Kt1zx31aqFjitfwidQnX2S1RdowsCNdWrjuzuW2i_KCjPy=s88-c-k-c0x00ffffff-no-rj" />
+      <img
+        src="https://yt3.ggpht.com/i3MfCW-U130eRmdBCD5XuYRlu0Kt1zx31aqFjitfwidQnX2S1RdowsCNdWrjuzuW2i_KCjPy=s88-c-k-c0x00ffffff-no-rj"
+        alt=""
+      />
     ),
   },
 ];
