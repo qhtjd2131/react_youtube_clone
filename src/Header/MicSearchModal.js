97c8ee6390ca25a0367e887c5ecd069e71abb1bf
qhtjd2131 <qhtjd2131@gmail.com>
2021-11-11 04:18:32 +0900

    micsearch modal styling

--- src/Header/MicSearchModal.js
+++ src/Header/MicSearchModal.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Overlay } from "../App";
 import "./MicSearchModal.scss";
+import { MdClose } from "react-icons/md";
+import { BsMicFill } from "react-icons/bs";
 
 const MicSearchModal = ({ setIsOpenMicSearch }) => {
   return (
@@ -13,12 +15,19 @@ const MicSearchModal = ({ setIsOpenMicSearch }) => {
       />
       <div className="modal-container">
         <div className="modal">
+          <div className="modal-ms-close-button">
+            <MdClose />
+          </div>
           <div className="modal-ms-title">음성으로 검색</div>
           <div className="modal-ms-sub-title">
             음성으로 검색하려면 브라우저 설정으로 이동해 마이크에 대한 액세스를
             허용하세요.
           </div>
-          <div className="modal-ms-icon">a</div>
+          <div className="modal-ms-icon-wrapper">
+            <div className="modal-ms-icon">
+              <BsMicFill />
+            </div>
+          </div>
         </div>
       </div>
     </>
