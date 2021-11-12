0916a7d71a95a7de0e4a836f03b0e019d80f924a
qhtjd2131 <qhtjd2131@gmail.com>
2021-11-12 15:07:14 +0900

    오류 수정 및 조건부랜더링으로 인한 state 초기화를 해결하기 위한 리팩토링 #2

--- src/Header/SettingDropdown.js
+++ src/Header/SettingDropdown.js
@@ -4,70 +4,78 @@ import { Line } from "../Side/SideBar";
 import * as data from "./HeaderData/SettingData.js";
 import { useOutSideClick } from "./AppMenuDropdown";
 
-const DefaultSettingDropdown = () => {
-  const { settingstate, setSettingState } = useContext(settingStateContext);
-  return data.settingDropdown_data.map((i, index) => (
-    <div key={index}>
-      <div
-        className="side-item"
-        onClick={(e) => {
-          if (i.nextPageState) {
-            setSettingState(i.nextPageState);
-          }
-        }}
-      >
-        <div className="setting-dropdown-item-icon">{i.image}</div>
-        <div className="setting-dropdown-item-label">{i.title}</div>
-        {i.nextPageState && <div className="next-page-button">{">"}</div>}
-      </div>
-      {index === 7 && <Line />}
-    </div>
-  ));
-};
-
 const SettingDesign = () => {
-  const { settingstate, setSettingState } = useContext(settingStateContext);
-  const [settingDesignState, setSettingDesignState] = useState("dark-theme");
+  const { settingState, setSettingState } = useContext(settingStateContext);
+  const [settingDesignState, setSettingDesignState] = useState("light-theme");
 
+  const s = {
+    display: "none",
+  };
   return (
-    <>
-      <div className="setting-prev-button-container">
-        <img
-          src="https://img.icons8.com/ios/50/000000/left.png"
-          alt=""
-          onClick={() => {
-            setSettingState(() => "default");
-          }}
-        />
-        <div className="setting-current-state-label">디자인</div>
-      </div>
-      <Line />
-      {data.DesignData.map((i, index) => (
-        <div key={index}>
-          <div
-            className="side-item"
+    settingState === "design" && (
+      <div>
+        <div className="setting-prev-button-container">
+          <img
+            src="https://img.icons8.com/ios/50/000000/left.png"
+            alt=""
             onClick={() => {
-              setSettingDesignState(() => i.settinDesignState);
+              setSettingState(() => "default");
             }}
-          >
-            <div className="setting-dropdown-item-icon">
-              {settingDesignState === i.settinDesignState ? (
-                i.image
-              ) : (
-                <div style={{ width: "24px", height: "24px" }} />
-              )}
-            </div>
-            <div className="setting-dropdown-item-label">{i.title}</div>
-          </div>
+          />
+          <div className="setting-current-state-label">디자인</div>
         </div>
-      ))}
-    </>
+        <Line />
+        {data.DesignData.map((i, index) => (
+          <div key={index}>
+            <div
+              className="side-item"
+              onClick={() => {
+                setSettingDesignState(() => i.settinDesignState);
+              }}
+            >
+              <div className="setting-dropdown-item-icon">
+                {settingDesignState === i.settinDesignState ? (
+                  i.image
+                ) : (
+                  <div style={{ width: "24px", height: "24px" }} />
+                )}
+              </div>
+              <div className="setting-dropdown-item-label">{i.title}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    )
   );
 };
 
 const SettingLanguage = () => {
   return;
 };
+const DefaultSettingDropdown = () => {
+  const { settingState, setSettingState } = useContext(settingStateContext);
+  return (
+    settingState === "default" &&
+    data.settingDropdown_data.map((i, index) => (
+      <div key={index}>
+        <div
+          className="side-item"
+          onClick={(e) => {
+            if (i.nextPageState) {
+              setSettingState(i.nextPageState);
+            }
+          }}
+        >
+          <div className="setting-dropdown-item-icon">{i.image}</div>
+          <div className="setting-dropdown-item-label">{i.title}</div>
+          {i.nextPageState && <div className="next-page-button">{">"}</div>}
+        </div>
+        {index === 7 && <Line />}
+      </div>
+    ))
+  );
+};
+
 const settingStateContext = createContext({});
 const SettingDropdown = ({ setIsOpenSettingDropdown }) => {
   const settingDropdownRef = createRef();
@@ -77,8 +85,8 @@ const SettingDropdown = ({ setIsOpenSettingDropdown }) => {
     <div className="setting-dropdown-container" ref={settingDropdownRef}>
       <div className="setting-dropdown">
         <settingStateContext.Provider value={{ settingState, setSettingState }}>
-          {settingState === "default" && <DefaultSettingDropdown />}
-          {settingState === "design" && <SettingDesign />}
+          <DefaultSettingDropdown />
+          <SettingDesign />
         </settingStateContext.Provider>
       </div>
     </div>
