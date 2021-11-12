b3acffacd52c9ecc9f45bd539998e8bb2b886973
qhtjd2131 <qhtjd2131@gmail.com>
2021-11-12 13:25:05 +0900

    setting dropdown에서 state 로 item 변경로직 구현

--- src/Header/HeaderData/SettingData.js
+++ src/Header/HeaderData/SettingData.js
@@ -7,28 +7,28 @@ export const settingDropdown_data = [
         alt=""
       />
     ),
-    nextpage: true,
+    nextPageState: "design",
   },
   {
     title: "언어: " + "한국어",
     image: (
       <img src="https://img.icons8.com/ios/50/000000/language.png" alt="" />
     ),
-    nextpage: true,
+    nextPageState: "language",
   },
   {
     title: "위치: " + "한국",
     image: (
       <img src="https://img.icons8.com/ios/50/000000/globe--v1.png" alt="" />
     ),
-    nextpage: true,
+    nextPageState: "location",
   },
   {
     title: "설정",
     image: (
       <img src="https://img.icons8.com/ios/50/000000/settings.png" alt="" />
     ),
-    nextpage: false,
+    nextPageState: null,
   },
   {
     title: "YouTube의 내 데이터",
@@ -38,26 +38,74 @@ export const settingDropdown_data = [
         alt=""
       />
     ),
-    nextpage: false,
+    nextPageState: null,
   },
   {
     title: "고객센터",
-    image: <img src="https://img.icons8.com/material-outlined/24/000000/help.png" alt=""/>,
-    nextpage: false,
+    image: (
+      <img
+        src="https://img.icons8.com/material-outlined/24/000000/help.png"
+        alt=""
+      />
+    ),
+    nextPageState: null,
   },
   {
     title: "의견 보내기",
-    image: <img src="https://img.icons8.com/windows/32/000000/about.png" alt=""/>,
-    nextpage: false,
+    image: (
+      <img src="https://img.icons8.com/windows/32/000000/about.png" alt="" />
+    ),
+    nextPageState: null,
   },
   {
     title: "단축키",
-    image: <img src="https://img.icons8.com/material-outlined/24/000000/keyboard.png" alt=""/>,
-    nextpage: false,
+    image: (
+      <img
+        src="https://img.icons8.com/material-outlined/24/000000/keyboard.png"
+        alt=""
+      />
+    ),
+    nextPageState: null,
   },
   {
     title: "제한모드: " + "사용안함",
     image: "",
-    nextpage: true,
+    nextPageState: "limitedMode",
+  },
+];
+
+export const DesignData = [
+//   {
+//     title: "기기 테마 사용",
+//     image: (
+//       <img
+//         src="https://img.icons8.com/external-becris-lineal-becris/64/000000/external-check-mintab-for-ios-becris-lineal-becris-1.png"
+//         alt=""
+//       />
+//     ),
+//     nextPageState: null,
+//     settinDesignState: "device-theme",
+//   },
+  {
+    title: "어두운 테마",
+    image: (
+      <img
+        src="https://img.icons8.com/external-becris-lineal-becris/64/000000/external-check-mintab-for-ios-becris-lineal-becris-1.png"
+        alt=""
+      />
+    ),
+    nextPageState: null,
+    settinDesignState: "dark-theme",
+  },
+  {
+    title: "밝은 테마",
+    image: (
+      <img
+        src="https://img.icons8.com/external-becris-lineal-becris/64/000000/external-check-mintab-for-ios-becris-lineal-becris-1.png"
+        alt=""
+      />
+    ),
+    nextPageState: null,
+    settinDesignState: "light-theme",
   },
 ];
--- src/Header/SettingDropdown.js
+++ src/Header/SettingDropdown.js
@@ -1,25 +1,86 @@
-import React, { createRef } from "react";
+import React, { createContext, createRef, useContext, useState } from "react";
 import "./SettingDropdown.scss";
 import { Line } from "../Side/SideBar";
 import * as data from "./HeaderData/SettingData.js";
 import { useOutSideClick } from "./AppMenuDropdown";
 
+const DefaultSettingDropdown = () => {
+  const { settingstate, setSettingState } = useContext(settingStateContext);
+  return data.settingDropdown_data.map((i, index) => (
+    <>
+      <div
+        className="side-item"
+        key={index}
+        onClick={(e) => {
+          if (i.nextPageState) {
+            setSettingState(i.nextPageState);
+          }
+        }}
+      >
+        <div className="setting-dropdown-item-icon">{i.image}</div>
+        <div className="setting-dropdown-item-label">{i.title}</div>
+        {i.nextPageState && <div className="next-page-button">{">"}</div>}
+      </div>
+      {index === 7 && <Line />}
+    </>
+  ));
+};
+
+const SettingDesign = () => {
+  const { settingstate, setSettingState } = useContext(settingStateContext);
+  const [settingDesignState, setSettingDesignState] = useState("light-theme");
+
+  return (
+    <>
+      <div className="setting-prev-button-container">
+        <img
+          src="https://img.icons8.com/ios/50/000000/left.png"
+          onClick={() => {
+            setSettingState(() => "default");
+          }}
+        />
+        <div className="setting-current-state-label">디자인</div>
+      </div>
+      <Line />
+      {data.DesignData.map((i, index) => (
+        <>
+          <div
+            className="side-item"
+            key={index}
+            onClick={() => {
+              setSettingDesignState(() => i.settinDesignState);
+            }}
+          >
+            <div className="setting-dropdown-item-icon">
+              {settingDesignState === i.settinDesignState ? (
+                i.image
+              ) : (
+                <div style={{ width: "24px", height: "24px" }} />
+              )}
+            </div>
+            <div className="setting-dropdown-item-label">{i.title}</div>
+          </div>
+        </>
+      ))}
+    </>
+  );
+};
+
+const SettingLanguage = () => {
+  return;
+};
+const settingStateContext = createContext({});
 const SettingDropdown = ({ setIsOpenSettingDropdown }) => {
   const settingDropdownRef = createRef();
+  const [settingState, setSettingState] = useState("default");
   useOutSideClick(settingDropdownRef, setIsOpenSettingDropdown);
   return (
     <div className="setting-dropdown-container" ref={settingDropdownRef}>
       <div className="setting-dropdown">
-        {data.settingDropdown_data.map((i, index) => (
-          <>
-            <div className="side-item" key={index}>
-              <div className="setting-dropdown-item-icon">{i.image}</div>
-              <div className="setting-dropdown-item-label">{i.title}</div>
-              {i.nextpage && <div className="next-page-button">{">"}</div>}
-            </div>
-            {index === 7 && <Line />}
-          </>
-        ))}
+        <settingStateContext.Provider value={{ settingState, setSettingState }}>
+          {settingState === "default" && <DefaultSettingDropdown />}
+          {settingState === "design" && <SettingDesign />}
+        </settingStateContext.Provider>
       </div>
     </div>
   );
