efecf54fd3da7fe4e64d42204c85ec8dee803d3c
qhtjd2131 <qhtjd2131@gmail.com>
2021-11-14 06:04:07 +0900

    sidebar 데이터 오타 및 불필요한 데이터 필터링

--- src/Side/SideData/data.js
+++ src/Side/SideData/data.js
@@ -12,28 +12,7 @@ import {
   BsExclamationSquare,
 } from "react-icons/bs";
 import { AiOutlineYoutube, AiOutlineSetting } from "react-icons/ai";
-export const item_mini = [
-  {
-    title: "홈",
-    image: <MdHomeFilled />,
-  },
-  {
-    title: "탐색",
-    image: <IoCompassOutline />,
-  },
-  {
-    title: "구독",
-    image: <MdOutlineSubscriptions />,
-  },
-  {
-    title: "보관함",
-    image: <MdShop2 />,
-  },
-  {
-    title: "시청 기록",
-    image: <MdOutlineHistory />,
-  },
-];
+
 export const language_side_item1 = {
   KOR: {
     home: "홈",
@@ -167,81 +146,6 @@ export const language_side_item3 = {
   },
 };
 
-export const item_3 = [
-  {
-    title: "음악",
-    image: (
-      <img
-        src="https://yt3.ggpht.com/96QoAiiyEzTweSqqNyWc7MfZLZk5xSgcPYuXB0aAdyxelNzU9kAco2GAPV8SeAhj3piG4whRCA=s88-c-k-c0x00ffffff-no-rj"
-        alt=""
-      />
-    ),
-  },
-  {
-    title: "스포츠",
-    image: (
-      <img
-        src="https://yt3.ggpht.com/jcGRECTvPeggGz4pxqerXrd2PYsjP38t1FeqBz3Vb937GTIuwwgybU4AYY6HwDlQHQt9iaoLlQ=s88-c-k-c0x00ffffff-no-rj"
-        alt=""
-      />
-    ),
-  },
-  {
-    title: "게임",
-    image: (
-      <img
-        src="https://yt3.ggpht.com/jQKnCfq2eoBd4EOaubdbvQ5Oe0uQ_CBA-qLRkw46ln1m8JDhkL8oBUwXX63ypSAhOGVj-V8T39g=s88-c-k-c0x00ffffff-no-rj"
-        alt=""
-      />
-    ),
-  },
-  {
-    title: "영화",
-    image: (
-      <img
-        src="https://yt3.ggpht.com/6lo97rUTO7xhIBXZqLiaW2kA_eMBIEmqc27EqlKLyE4nAY-yzcKBG0Hs0YdUka3gJ629HcwgyzQ=s88-c-k-c0x00ffffff-no-rj"
-        alt=""
-      />
-    ),
-  },
-  {
-    title: "뉴스",
-    image: (
-      <img
-        src="https://yt3.ggpht.com/pZxo_dJtADS-AkEx1mnPpc49paUy_aQywNgHju68VLQRMWJ4z30_thCXaNzERG8Vn74xPS-LLw=s88-c-k-c0x00ffffff-no-rj"
-        alt=""
-      />
-    ),
-  },
-  {
-    title: "실시간",
-    image: (
-      <img
-        src="https://yt3.ggpht.com/8D6JlsnvwDZFMdcbjqVji82kggP3aXXbO-yBD0RFrKlp4G1zNt9wcqcVTSPnAI8GuUAbDYQwsg=s88-c-k-c0x00ffffff-no-rj"
-        alt=""
-      />
-    ),
-  },
-  {
-    title: "학습",
-    image: (
-      <img
-        src="https://yt3.ggpht.com/__Mlk4S9hJAiS34gzzVBqpY-_9tc6m4nIZGk0cMseq5q_Ux51MyhcBWYIPfXmqCCfazU_5hniQ=s88-c-k-c0x00ffffff-no-rj"
-        alt=""
-      />
-    ),
-  },
-  {
-    title: "360 동영상",
-    image: (
-      <img
-        src="https://yt3.ggpht.com/i3MfCW-U130eRmdBCD5XuYRlu0Kt1zx31aqFjitfwidQnX2S1RdowsCNdWrjuzuW2i_KCjPy=s88-c-k-c0x00ffffff-no-rj"
-        alt=""
-      />
-    ),
-  },
-];
-
 export const data_side_item4 = {
   browseChnnels: {
     image: <BsPlusCircle />,
@@ -256,12 +160,6 @@ export const language_side_item4 = {
     browseChnnels: "Browse channels",
   },
 };
-export const item_4 = [
-  {
-    title: "채널 탐색",
-    image: <BsPlusCircle />,
-  },
-];
 
 export const data_side_item5 = {
   outubePremium: {
@@ -296,31 +194,3 @@ export const language_side_item5 = {
     sendFeedback: "Send feedback",
   },
 };
-export const item_5 = [
-  {
-    title: "YouTube Premium",
-    image: <AiOutlineYoutube />,
-  },
-  {
-    title: "실시간",
-    image: <IoRadioOutline />,
-  },
-];
-export const item_6 = [
-  {
-    title: "설정",
-    image: <AiOutlineSetting />,
-  },
-  {
-    title: "신고 기록",
-    image: <MdOutlineOutlinedFlag />,
-  },
-  {
-    title: "고객센터",
-    image: <BsQuestionCircle />,
-  },
-  {
-    title: "의견 보내기",
-    image: <BsExclamationSquare />,
-  },
-];
