41e4d4bef5353c42655aa5e25ac88ad8b37bfb99
qhtjd2131 <qhtjd2131@gmail.com>
2021-11-14 17:32:49 +0900

    themeState에 따른 테마변경을 위한 리팩토링 #5, searchWithVoice Modal darkTheme 구현

--- src/Header/MicSearchModal.js
+++ src/Header/MicSearchModal.js
@@ -3,9 +3,10 @@ import { Overlay } from "../App";
 import "./MicSearchModal.scss";
 
 import * as data from "./HeaderData/headerBarData.js";
-import { languageStateContext } from "../App";
+import { languageStateContext, themeStateContext } from "../App";
 const MicSearchModal = ({ setIsOpenMicSearch }) => {
   const { languageState } = useContext(languageStateContext);
+  const { themeState } = useContext(themeStateContext);
   return (
     <>
       <Overlay
@@ -14,7 +15,7 @@ const MicSearchModal = ({ setIsOpenMicSearch }) => {
         }}
       />
       <div className="modal-container">
-        <div className="modal">
+        <div className={"modal "+"modal-"+themeState}>
           <div
             className="modal-ms-close-button"
             onClick={() => {
@@ -30,7 +31,7 @@ const MicSearchModal = ({ setIsOpenMicSearch }) => {
             {data.language_Search[languageState].searchWithYourVoiceDescription}
           </div>
           <div className="modal-ms-icon-wrapper">
-            <div className="modal-ms-icon">
+            <div className={"modal-ms-icon "+"modal-ms-icon-"+themeState }>
               {data.data_Search.searchWithYourVoice.image}
             </div>
           </div>
