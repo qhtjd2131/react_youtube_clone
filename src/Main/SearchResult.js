e11ace3131bf85bad70ff84188a81c23f46c571a
qhtjd2131 <qhtjd2131@gmail.com>
2021-11-19 17:13:53 +0900

    query string parsing 구현

--- src/Main/SearchResult.js
+++ src/Main/SearchResult.js
@@ -1,23 +1,35 @@
-import React from "react";
-import { useLocation, useParams } from "react-router";
-import { useEffect, useState } from "react/cjs/react.development";
+import React, { useContext, useEffect, useState } from "react";
+import { useLocation } from "react-router";
 import "./SearchResult.scss";
+import { themeStateContext, SideBarContext } from "../App";
 
 const SearchResult = () => {
   const [queryString, setQueryString] = useState();
   const location = useLocation();
-  console.log(location);
+  const { themeState } = useContext(themeStateContext);
+  const { isOpenSideBar, isWindowSizeXL } = useContext(SideBarContext);
+
+  //   console.log(location);
 
   useEffect(() => {
-    let query = location.search.slice(1).split("&");
-    setQueryString(() => decodeURI(location.search).slice(1).split("&"));
-    console.log("query:", query);
+    setQueryString(() => {
+      //query string이 한개일때만 가정
+      let q = decodeURI(location.search).slice(1); // [q=apple]
+      let firstEqualIndex = q.indexOf("=");
+      const key = q.slice(0, firstEqualIndex); //q
+      const value = q.slice(firstEqualIndex + 1); //apple
+      return value;
+    });
   }, [location.search]);
   return (
-    <div className="search-result">
-      <div>hello world im search result</div>
-      <br />
-      <div>{queryString}</div>
+    <div
+      className={
+        isOpenSideBar && isWindowSizeXL
+          ? "main main-" + themeState
+          : "main side-close-main main-" + themeState
+      }
+    >
+      <div className="search-result-container">hello</div>
     </div>
   );
 };
